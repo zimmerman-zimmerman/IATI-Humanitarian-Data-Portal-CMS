44a8726a6984c8cd3606b3c8503e061915d605e5
fus <martynas@zimmermanzimmerman.nl>
2019-08-16 12:16:28 +0300

    chore: adjusted sign-in storybook

--- src/app/modules/SignIn/story.tsx
+++ src/app/modules/SignIn/story.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 
-import Module from '.';
+import { SignIn } from '.';
 import Providers from 'app/Providers';
 
 storiesOf('Modules|Sign in', module).add('layout', () => (
-  <div style={{ width: "100%", height: "100%" }}>
+  <div style={{ width: '100%', height: '100%' }}>
     <Providers>
-      <Module />
+      <SignIn />
     </Providers>
   </div>
 ));
